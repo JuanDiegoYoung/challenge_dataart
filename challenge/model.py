c4892f5c7707d530a104d18cc58119cc5ef05566
JuanDiegoYoung <young.juandiego@gmail.com>
2026-05-20 15:24:07 -0300

    implement delay prediction model

--- challenge/model.py
+++ challenge/model.py
@@ -1,6 +1,21 @@
-import pandas as pd
+from typing import List, Tuple, Union
 
-from typing import Tuple, Union, List
+import pandas as pd
+import xgboost as xgb
+
+
+FEATURE_COLUMNS = [
+    "OPERA_Latin American Wings",
+    "MES_7",
+    "MES_10",
+    "OPERA_Grupo LATAM",
+    "MES_12",
+    "TIPOVUELO_I",
+    "MES_4",
+    "MES_11",
+    "OPERA_Sky Airline",
+    "OPERA_Copa Air",
+]
 
 class DelayModel:
 
@@ -26,7 +41,24 @@ class DelayModel:
             or
             pd.DataFrame: features.
         """
-        return
+        features = pd.concat(
+            [
+                pd.get_dummies(data["OPERA"], prefix="OPERA"),
+                pd.get_dummies(data["TIPOVUELO"], prefix="TIPOVUELO"),
+                pd.get_dummies(data["MES"], prefix="MES"),
+            ],
+            axis=1,
+        )
+        features = features.reindex(columns=FEATURE_COLUMNS, fill_value=0)
+
+        if target_column is None:
+            return features
+
+        data = data.copy()
+        if target_column not in data.columns:
+            data[target_column] = self._get_delay_target(data)
+
+        return features, data[[target_column]]
 
     def fit(
         self,
@@ -40,7 +72,17 @@ class DelayModel:
             features (pd.DataFrame): preprocessed data.
             target (pd.DataFrame): target.
         """
-        return
+        negative_count = int((target.iloc[:, 0] == 0).sum())
+        positive_count = int((target.iloc[:, 0] == 1).sum())
+        scale_pos_weight = negative_count / positive_count
+
+        self._model = xgb.XGBClassifier(
+            random_state=1,
+            learning_rate=0.01,
+            scale_pos_weight=scale_pos_weight,
+            eval_metric="logloss",
+        )
+        self._model.fit(features, target.iloc[:, 0])
 
     def predict(
         self,
@@ -55,4 +97,14 @@ class DelayModel:
         Returns:
             (List[int]): predicted targets.
         """
-        return
+        if self._model is None:
+            return [0] * features.shape[0]
+
+        return [int(prediction) for prediction in self._model.predict(features)]
+
+    @staticmethod
+    def _get_delay_target(data: pd.DataFrame) -> pd.Series:
+        fecha_o = pd.to_datetime(data["Fecha-O"])
+        fecha_i = pd.to_datetime(data["Fecha-I"])
+        min_diff = (fecha_o - fecha_i).dt.total_seconds() / 60
+        return (min_diff > 15).astype(int)
